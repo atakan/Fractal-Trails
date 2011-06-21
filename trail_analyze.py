2889d288a611db7a3bb00d2f8d2fb6c628357f26
M Atakan Gurkan <ato.gurkan@gmail.com>
2011-06-21 16:37:16 +0200

    added license terms

--- trail_analyze.py
+++ trail_analyze.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+#    Copyright (C) 2011  Mehmet Atakan Gürkan
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License version 3 as
+#    published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program (probably in a file named COPYING).
+#    If not, see <http://www.gnu.org/licenses/>.
+
 from __future__ import print_function, division
 import numpy as np
 import matplotlib.pyplot as plt
-#from pylab import *
-#from scipy import *
 
 import sys
 import argparse
